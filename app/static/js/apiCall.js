b19a73dc08046d21f58e6b210322e8bc500e8c45
Mateusz kielb <relextm19@gmail.com>
2024-10-24 20:23:03 +0200

    display implemented full vanila js and golang

--- app/static/js/apiCall.js
+++ app/static/js/apiCall.js
@@ -8,3 +8,12 @@ export async function getLanguagesData() {
     console.log('Fetched data:', data);
     return data;
 }
+export async function getProjectsData() {
+    const response = await fetch(URL + "getProjects");
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log('Fetched data:', data);
+    return data;
+}
--- app/static/js/projects.js
+++ app/static/js/projects.js
@@ -1,57 +1,40 @@
-import { getLanguagesData } from "./apiCall.js";
+import { getProjectsData } from "./apiCall.js";
 import { format_time } from "./parse.js";
 
+
 var totalTime = 0;
-
 document.addEventListener('DOMContentLoaded', async () => {
-    let languages = await getLanguagesData();
+    let projects = await getProjectsData();
 
-    let languagesArray = Object.entries(languages);
-    //the b[1] - a[1] is to sort the array in descending order it compares adjecent elements and if the result is negative it swaps them
-    languagesArray.sort((a, b) => b[1] - a[1]);
-    let sortedLanguages = Object.fromEntries(languagesArray);
-
-    renderLanguages(sortedLanguages);
+    renderProjects(projects);
 
     //set the total time
     var totalTimeElement = document.querySelector('.total-time-p');
     totalTimeElement.textContent = format_time(totalTime);
 });
-function renderLanguages(languages){
-    for(let [name, time] of Object.entries(languages)){
-        
-        //create the wrapper
-        let langWrapDiv = document.createElement('div');
-        langWrapDiv.classList.add('lang-wrap');
-        //create the image and name div wrap
-        let imgNameWrapDiv = document.createElement('div');
-        imgNameWrapDiv.classList.add('img-name-wrap');
-        
-        //create the image element
-        let langImg = document.createElement('img');
-        langImg.src = "../static/img/" + name + ".png";
-        langImg.alt = name;
+function renderProjects(projects){
+    for(let [name,time] of Object.entries(projects)){
 
-        //create the name element
-        let langName = document.createElement('h3');
-        langName.classList.add('lang-name');
-        langName.textContent = name;
-            
-        //append image and name to the img-name-wrap
-        imgNameWrapDiv.appendChild(langImg);
-        imgNameWrapDiv.appendChild(langName);
+        //create wrapper
+        let wrapDiv = document.createElement('div');
+        wrapDiv.classList.add('project-wrap');
+        //create project-name
+        let projectName = document.createElement('h3');
+        projectName.classList.add('project-name')
+        projectName.innerHTML = (`${name}: &nbsp;`);
+        //create project-time
+        let projectTime = document.createElement('h3');
+        projectTime.classList.add('project-time')
+        projectName.classList.add('time-div')
+        projectTime.textContent = format_time(time);
+        //add children
+        wrapDiv.appendChild(projectName);
+        wrapDiv.appendChild(projectTime);
 
-        //create the time element
-        let langTime = document.createElement('h3');
-        langTime.classList.add('lang-time');
-        langTime.textContent = format_time(time);
-        //update the global total time
+        //append to document
+        document.querySelector('.lang-stats').appendChild(wrapDiv);
+
+        //update total time
         totalTime += time;
-
-        //append img-name-wrap and time-element to lang-wrap
-        langWrapDiv.appendChild(imgNameWrapDiv);
-        langWrapDiv.appendChild(langTime);
-
-        document.querySelector('.text-content').appendChild(langWrapDiv);
     }
 }
