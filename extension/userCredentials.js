4599cf486f4ad8abe8cdd40e3f8f379c33ebf241
relextm19 <relextm19@gmail.com>
2025-03-27 12:53:04 +0100

    register through vscode added

--- extension/userCredentials.js
+++ extension/userCredentials.js
@@ -25,20 +25,41 @@ async function promptUserCredentials(context) {
   if(emailUsed){
     while (true) {
       let password = await promptPassword();
-      if (password.trim() === '') {
-        vscode.window.showErrorMessage('Password cannot be empty.');
-        continue;
-      }
-      if (!password) {
-        return;
-      }
+      if(password === undefined) return;
+      if(checkPasswordEmpty(password)) continue;
+
       if(await login(email, password, context)) return;
+      
       else{
         vscode.window.showErrorMessage('Invalid email or password');
         continue;
       };
     }
+  } else{
+    while (true){
+      const password1 = await promptPassword("Enter new password");
+      if(password1 === undefined) return;
+      if(checkPasswordEmpty(password1)) continue;
+      
+      const password2 = await promptPassword("Confirm new password");
+      if(password2 === undefined) return;
+      
+      if(password1 !== password2){
+        vscode.window.showErrorMessage('Passwords do not match');
+        continue;
+      }
+      register(email, password1, context);
+      return;
+    }    
   }
+}
+
+function checkPasswordEmpty(password){
+  if (password.trim() === '') {
+    vscode.window.showErrorMessage('Password cannot be empty.');
+    return true;
+  }
+  return false;
 }
 
 async function promptEmail() {
@@ -53,13 +74,14 @@ async function promptEmail() {
   return emailRegex.test(email) ? email : null;
 }
 
-async function promptPassword() {
+async function promptPassword(prompt) {
   const password = await vscode.window.showInputBox({
-    prompt: 'Enter your password',
+    prompt: prompt || 'Enter your password',
     placeHolder: 'Password',
     password: true,
     ignoreFocusOut: true,
   });
+  
   return password;
 }
 
@@ -97,6 +119,28 @@ async function login(email, password, context) {
   } catch (error) {
     vscode.window.showErrorMessage('Failed to login');
     console.error('Login error:', error);
+    return false;
+  }
+}
+async function register(email, password, context) {
+  const payload = { email, password };
+  try {
+    const request = await fetch('http://127.0.0.1:8080/signUp', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(payload),
+    });
+    if(!request.ok){
+      vscode.window.showErrorMessage('Invalid email or password');
+      return false;
+    }
+    // Save credentials for future runs.
+    await context.globalState.update('userCredentials', { email, password });
+    vscode.window.showInformationMessage('Account registered.');
+    return true;
+  } catch (error) {
+    vscode.window.showErrorMessage('Failed to register account');
+    console.error('register error:', error);
     return false;
   }
 }
