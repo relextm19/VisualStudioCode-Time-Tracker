9a35f297f55b505f2b3412b2f35a8276689a52d7
relextm19 <relextm19@gmail.com>
2025-09-09 16:55:48 +0200

    progress bar added

--- app/frontend/site/src/Home.vue
+++ app/frontend/site/src/Home.vue
@@ -1,7 +1,5 @@
 <template>
-    <TotalTimeDisplay :total-time="13002400"></TotalTimeDisplay>    
 </template>
 
 <script setup lang="ts">
-    import TotalTimeDisplay from './components/TotalTimeDisplay.vue';
 </script>
