281eb3cff2cd32498a221b56da1dee259879dbba
relextm19 <relextm19@gmail.com>
2025-10-20 22:57:26 +0200

    added a redirect for logged in users trying to acces public paths

--- app/frontend/site/src/router.ts
+++ app/frontend/site/src/router.ts
@@ -3,6 +3,7 @@ import Login from './Login.vue';
 import Home from './Home.vue';
 import Register from './Register.vue';
 import Languages from './Languages.vue';
+import { log } from 'console';
 
 const routes = [
     { path: '/login', component: Login },
@@ -15,13 +16,22 @@ const router = createRouter({
     history: createWebHistory(),
     routes,
 });
+
+let loggedIn = false;// cache the value so i dont have to hit /api/checkAuth every request. This is not a security issue cause i still have an auth middleware on the backend so if someone plays arround with the client side the app still wont let them acces data
+
 router.beforeEach(async (to, _, next) => {
     const publicPaths = ['/login', '/register'];
+
+    if(publicPaths.includes(to.path)){
+        if (loggedIn) return next('/');
+        return next();
+    }
+    if(loggedIn) return next();
 
     try {
         const res = await fetch('/api/checkAuth', { credentials: 'include' });
         if (res.ok) {
-            if (publicPaths.includes(to.path)) return next('/');
+            loggedIn = true;
             next();
         }
         else next('/login');
