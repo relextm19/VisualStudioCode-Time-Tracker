947c0bd1b5e55f2d7f2f12b912b222585f81272b
relextm19 <relextm19@gmail.com>
2025-10-20 22:03:23 +0200

    added a display switch element

--- app/frontend/site/src/Languages.vue
+++ app/frontend/site/src/Languages.vue
@@ -1,35 +1,36 @@
 <template>
-  <TotalTimeDisplay :totalTime="totalTime" />
-
-  <div v-if="languages.length > 0">
-    <div v-for="language in languages" :key="language.name">
-      <LanguageTimeDisplay 
-        :languageName="language.name" 
-        :totalTime="language.time" 
-      />
+    <TotalTimeDisplay :totalTime="totalTime" />
+    <DisplaySwitch />
+    <div v-if="languages.length > 0">
+        <div v-for="language in languages" :key="language.name">
+            <LanguageTimeDisplay 
+                :languageName="language.name" 
+                :totalTime="language.time" 
+            />
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import TotalTimeDisplay from './components/TotalTimeDisplay.vue'
 import LanguageTimeDisplay from './components/LanguageTimeDisplay.vue'
+import DisplaySwitch from './components/DisplaySwitch.vue'
 
 interface timeData {
-  name: string
-  time: number
+    name: string;
+    time: number;
 }
 
-const projects = ref<timeData[]>([])
-const languages = ref<timeData[]>([])
-const totalTime = ref(0)
+const projects = ref<timeData[]>([]);
+const languages = ref<timeData[]>([]);
+const totalTime = ref(0);
 
 onMounted(async () => {
-  const response = await fetch('/api/userMetrics')
-  const json = await response.json() 
-  projects.value = json.projects
-  languages.value = json.languages
-  totalTime.value = json.totalTime 
+    const response = await fetch('/api/userMetrics');
+    const json = await response.json(); 
+    projects.value = json.projects;
+    languages.value = json.languages;
+    totalTime.value = json.totalTime;
 })
 </script>
---
+++ app/frontend/site/src/components/DisplaySwitch.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="w-min rounded-2xl flex justify-items-center gap-2 border-2 border-white relative">
+        <div 
+            class="absolute bg-white w-1/2 h-full rounded-2xl pointer-events-none transition-transform duration-200 ease-out"
+            :class="showLanguages ? '' : 'translate-x-full'"
+        ></div>
+        <span 
+            :class="showLanguages ? 'text-black' : 'text-white'"
+            class="p-0.5 z-10"
+            @click="showLanguages = true; $emit('changeDisplay')"
+        >Languages</span>
+        <span 
+            :class="showLanguages ? 'text-white' : 'text-black'"
+            class="p-0.5 z-10"
+            @click="showLanguages = false; $emit('changeDisplay')"
+        >Projects</span>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { ref } from 'vue';
+
+    const showLanguages = ref(true)
+</script>
