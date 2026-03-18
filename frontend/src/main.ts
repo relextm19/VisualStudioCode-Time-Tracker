0b44cacb8fb7c59eef4af8386fd1d075ed59498e
Mateusz Kielb <relextm19@gmail.com>
2026-03-18 18:59:05 +0100

    moved the web page frontend to another repo

--- frontend/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './styles/tailwind.css'
-import router from './router.ts'
-
-createApp(App).use(router).mount('#app');
--- frontend/src/utils/formatTime.ts
+++
@@ -1,7 +0,0 @@
-export function formatTime(time: number): [number, number, number] {
-  const hours = Math.floor(time / 3600);
-  const minutes = Math.floor((time % 3600) / 60);
-  const seconds = time % 60;
-
-  return [hours, minutes, seconds];
-}
